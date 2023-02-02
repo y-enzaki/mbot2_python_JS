7facd65ad919771c820f80b62a07a245aadf3886
Yuki ENZAKI <63327815+yezark@users.noreply.github.com>
2023-02-02 14:00:53 +0900

    websock微修正

--- playcanvas_websocket.js
+++ playcanvas_websocket.js
@@ -1,9 +1,9 @@
 
 var Websock = pc.createScript('playcanvas_websocket');
-
+var ws;
 Websock.attributes.add('server', { type: 'string', default: "ws://localhost:9999/"});
 Websock.prototype.initialize = function () {
-    let ws = new WebSocket(this.server);
+    ws = new WebSocket(this.server);
     ws.onmessage = function (message) {
         console.log(message.data);
     }
