5172d974ac32af91011d3ac02224674436e6125d
FLOW-X13-ENZAKI\yezar <y-enzaki@musashino-u.ac.jp>
2023-06-09 16:43:41 +0900

    汎用化処理の途中

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import cyberpi
 from websocket_server import WebsocketServer
 from datetime import datetime
 
+import json
+
 def new_client(client, server):
     server.send_message_to_all(datetime.now().isoformat() + ": new client joined!")
     cyberpi.console.print("connected!\n")
@@ -10,20 +12,48 @@ def new_client(client, server):
     return
 
 def on_message(client, server,message):
-    cyberpi.console.print(message+"\n")
+    #cyberpi.console.print(message+"\n")
     print(message)
-    if(message == "forward"):
-        cyberpi.mbot2.EM_set_speed(10,"em1")
-        cyberpi.mbot2.EM_set_speed(-10,"em2")
-    elif(message=="backward"):
-        cyberpi.mbot2.EM_set_speed(-10,"em1")
-        cyberpi.mbot2.EM_set_speed(10,"em2")        
-    elif(message=="stop"):
-        cyberpi.mbot2.EM_set_speed(0,"all")
+    recvdata = json.loads(message)
+    if("command" in recvdata):
+        if(recvdata["command"] == "forward"):
+            cyberpi.mbot2.EM_set_speed(10,"em1")
+            cyberpi.mbot2.EM_set_speed(-10,"em2")
+        elif(recvdata["command"]=="backward"):
+            cyberpi.mbot2.EM_set_speed(-10,"em1")
+            cyberpi.mbot2.EM_set_speed(10,"em2")        
+        elif(recvdata["command"]=="stop"):
+            cyberpi.mbot2.EM_stop("all")
+        elif(recvdata["command"]=="reset"):
+            cyberpi.mbot2.EM_reset_angle("all")
+        elif(recvdata["command"]=="setpower"):
+            if("power1" in recvdata):
+                cyberpi.mbot2.EM_set_power(recvdata["power1"],"em1")
+            if("power2" in recvdata):
+                cyberpi.mbot2.EM_set_power(recvdata["power2"],"em2")
+        elif(recvdata["command"]=="turn"):
+            speed1 = 10
+            speed2 = 10
+            if("speed1" in recvdata):
+                speed1=recvdata["speed1"]
+            if("speed2" in recvdata):
+                speed2=recvdata["speed2"]
+            if("angle1" in recvdata):
+                cyberpi.mbot2.EM_turn(recvdata["angle1"],speed1,"em1")
+            if("angle2" in recvdata):
+                cyberpi.mbot2.EM_turn(recvdata["angle2"],speed2,"em2")   
 
     distance = cyberpi.ultrasonic2.get()
-    print(distance)
-    server.send_message(client,str(distance))
+    enc1 = cyberpi.mbot2.EM_get_angle("em1")
+    enc2 = cyberpi.mbot2.EM_get_angle("em2")
+    stat = {
+        "usdis":distance,
+        "enc1":enc1,
+        "enc2":enc2,
+    }
+    senddata= json.dumps(stat)
+    print(senddata)
+    server.send_message(client,senddata)
     return
 
 server = WebsocketServer("localhost",9999)
--- websock.js
+++ websock.js
@@ -1,15 +1,45 @@
 let ws = new WebSocket("ws://localhost:9999/");
 let log = "";
+let senddata = {};
+let recvdata = {};
 ws.onmessage = function (message) {
     log = log + "<br>" + message.data;
     document.getElementById("txt").innerHTML = log;
 }
 document.getElementById("forward").addEventListener("click", function () {
-     ws.send("forward");
+    let com = { command: "forward" };
+    ws.send(JSON.stringify(com));
+    //ws.send("forward");
  });
- document.getElementById("stop").addEventListener("click", function () {
-    ws.send("stop");
+document.getElementById("stop").addEventListener("click", function () {
+    let com = { command: "stop" };
+    ws.send(JSON.stringify(com));
+    //ws.send("stop");
 });
- document.getElementById("backward").addEventListener("click", function () {
-    ws.send("backward");
+document.getElementById("backward").addEventListener("click", function () {
+    let com = { command: "backward" };
+    ws.send(JSON.stringify(com));
+    //ws.send("backward");
+});
+document.getElementById("fullpower").addEventListener("click", function () {
+    let com = {
+        command: "setpower",
+        power1: 100,
+    power2:100,
+    };
+    ws.send(JSON.stringify(com));
+    //ws.send("backward");
+});
+document.getElementById("turn90").addEventListener("click", function () {
+    let com = {
+        command: "turn",
+        angle1: 90,
+    };
+    ws.send(JSON.stringify(com));
+    //ws.send("backward");
+});
+document.getElementById("reset").addEventListener("click", function () {
+    let com = { command: "reset" };
+    ws.send(JSON.stringify(com));
+    //ws.send("backward");
 });
